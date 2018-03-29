9d708d402e2596bd5b77808e01579cd142696232
wangathan <wangathan@gmail.com>
2018-03-29 15:56:20 -0400

    added script to generate prediction/build batch scripts

---
+++ determineTilesToPredict.R
@@ -0,0 +1,59 @@
+#################
+#
+#   Analyze tiles that are fully featured and fully predicted in order
+#     to generate batch predict/mapping script.
+#
+#
+#
+
+library(data.table)
+library(parallel)
+
+rfid = commandArgs(TRUE)[1]
+rfid = "tc_20180319_k50_pam_rf"
+
+checker=function(tile,type){
+  if(type=="feats"){
+    inpath = feats[grep(tile,feats)]
+    numfi = length(list.files(inpath))
+  }
+  if(type=="preds"){
+    inpath = preds[grep(tile,preds)]
+    numfi = length(list.files(inpath))
+  }
+  return(numfi==3000)
+}
+
+## get list of features
+## determine which features are complete
+
+feats = list.files("../../data/features/",
+                   pattern="Bh.*[0-9]$",
+                   full.names=T)
+
+featdt = data.table(tilefeats = basename(feats))
+
+featcompleted=mclapply(featdt$tilefeats,checker,type="feats",mc.cores=detectCores())
+featdt[,completed:=featcompleted]
+
+finishedfeat=featdt[completed==T,tilefeats]
+
+## get list of predictions
+## determine which ones are complete
+
+preds= list.files(paste0("../../data/rf/predict_",rfid),
+                   pattern="Bh.*[0-9]$",
+                   full.names=T)
+
+preddt = data.table(tilepreds = basename(preds))
+
+predcompleted=mclapply(preddt$tilepreds,checker,type="preds",mc.cores=detectCores())
+preddt[,completed:=predcompleted]
+
+finishedpred=preddt[completed==T, tilepreds]
+
+## subtract
+
+predToDo = finishedfeat[! finishedfeat %in% finishedpred]
+
+## 
